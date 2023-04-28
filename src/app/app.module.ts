cd721d2efa03c80373fa0303fdf2276c3cc3dde9
jenniffergon <j.gonzalezg2@uniandes.edu.co>
2023-04-28 11:14:41 -0500

    Crear servicio HTTP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -11,7 +12,8 @@ import { CafesModule } from './cafes/cafes.module';
   ],
   imports: [
     BrowserModule,
-    CafesModule
+    CafesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cafes/cafes.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { Cafe } from './cafe';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CafesService {
+
+constructor(private http: HttpClient) { }
+
+  getCafe(): Observable<Cafe[]> {
+    return this.http.get<Cafe[]>(environment.baseUrl);
+  }
+}
