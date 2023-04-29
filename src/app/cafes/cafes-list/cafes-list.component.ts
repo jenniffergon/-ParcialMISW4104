f5f391584e2e386cb60c8e6e3f036bb912970d52
jenniffergon <j.gonzalezg2@uniandes.edu.co>
2023-04-28 21:07:50 -0500

    Modificación del componente para listar cafés

--- src/app/cafes/cafes-list/cafes-list.component.ts
+++ src/app/cafes/cafes-list/cafes-list.component.ts
@@ -10,6 +10,9 @@ import { CafesService } from '../cafes.service';
 export class CafesListComponent implements OnInit {
 
   cafe: Array<Cafe> = [];
+  origen: number = 0;
+  blend: number = 0;
+
   constructor(private CafeService: CafesService) {}
     getCafes(): void{
       this.CafeService.getCafe().subscribe((cafe) =>{
@@ -17,8 +20,23 @@ export class CafesListComponent implements OnInit {
       });
    }
 
+   tiposCafe(){
+    this.CafeService.getCafe().subscribe((cafe) =>{
+      this.cafe = cafe;
+      for (let elemento of this.cafe) {
+        if(elemento["tipo"]=="Café de Origen"){
+          this.origen = this.origen+1;
+        }
+        if(elemento["tipo"]=="Blend"){
+          this.blend = this.blend+1;
+        }
+       }
+    });
+   }
+
   ngOnInit() {
     this.getCafes();
+    this.tiposCafe();
   }
 
 }
